c296a51c8256e86c26b359fb328f62c9a6dc1db0
Madoka Fujimaki Ahlsèn <madoka@bazooka.se>
2017-03-27 16:02:53 +0200

    Added FAQ page

---
+++ PizzeriaEpiserverSite/Models/Pages/FAQItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace PizzeriaEpiserverSite.Models.Pages
+{
+    [ContentType(DisplayName = "FAQItem", GUID = "b42c0a0c-ddfd-4910-af58-fdde6b8bb97f", Description = "", AvailableInEditMode = false)]
+    public class FAQItem : SitePageData
+    {
+        [Display(
+            Name = "Question",
+            GroupName = SystemTabNames.Content,
+            Order = 10)]
+        public virtual string Question { get; set; }
+
+        [Display(
+            Name = "Answer",
+            GroupName = SystemTabNames.Content,
+            Order = 20)]
+        public virtual string Answer { get; set; }
+    }
+}
---
+++ PizzeriaEpiserverSite/Models/Pages/FAQPage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+using System.Collections.Generic;
+
+
+namespace PizzeriaEpiserverSite.Models.Pages
+{
+    [ContentType(DisplayName = "FAQPage", GUID = "a4c86c31-ac81-44ed-9f7f-547bff8124ad", Description = "")]
+    public class FAQPage : SitePageData
+    {
+        [Ignore]
+        public IList<FAQItem> FAQItems { get; set; }
+    }
+}
