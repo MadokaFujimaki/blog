fafaabcf3a79607dc6ccf1064195b90cb53cbe92
Madoka Fujimaki Ahlsèn <madoka@bazooka.se>
2017-05-03 13:33:58 +0200

    FIxed bug on blogPage

--- PizzeriaEpiserverSite/Controllers/BlogCateoryListPageController.cs
+++ PizzeriaEpiserverSite/Controllers/BlogCateoryListPageController.cs
@@ -1,26 +1,26 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Web.Mvc;
-using EPiServer;
-using EPiServer.Core;
-using EPiServer.Framework.DataAnnotations;
-using EPiServer.Web.Mvc;
-using PizzeriaEpiserverSite.Business;
-using PizzeriaEpiserverSite.Models.Pages;
-using PizzeriaEpiserverSite.Models.ViewModels;
+﻿//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web.Mvc;
+//using EPiServer;
+//using EPiServer.Core;
+//using EPiServer.Framework.DataAnnotations;
+//using EPiServer.Web.Mvc;
+//using PizzeriaEpiserverSite.Business;
+//using PizzeriaEpiserverSite.Models.Pages;
+//using PizzeriaEpiserverSite.Models.ViewModels;
 
-namespace PizzeriaEpiserverSite.Controllers
-{
-    public class BlogCateoryListPageController : PageController<BlogPage>
-    {
-        public ActionResult Index(BlogPage currentPage)
-        {
-            var model = new BlogCateoryListViewModel
-            {
-                category = (currentPage as ICategorizable).Category.GetCategories().FirstOrDefault()
-            };
+//namespace PizzeriaEpiserverSite.Controllers
+//{
+//    public class BlogCateoryListPageController : PageController<BlogPage>
+//    {
+//        public ActionResult Index(BlogPage currentPage)
+//        {
+//            var model = new BlogCateoryListViewModel
+//            {
+//                category = (currentPage as ICategorizable).Category.GetCategories().FirstOrDefault()
+//            };
 
-            return PartialView(model);
-        }
-    }
-}
+//            return PartialView(model);
+//        }
+//    }
+//}
