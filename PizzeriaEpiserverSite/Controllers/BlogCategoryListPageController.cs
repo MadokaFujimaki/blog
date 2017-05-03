34da9f6e8cf7efca8290bbe4d263cec4009bcb0c
Madoka Fujimaki Ahlsèn <madoka@bazooka.se>
2017-05-03 15:20:36 +0200

    Added category list

---
+++ PizzeriaEpiserverSite/Controllers/BlogCategoryListPageController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.Framework.DataAnnotations;
+using EPiServer.ServiceLocation;
+using EPiServer.Web.Mvc;
+using PizzeriaEpiserverSite.Business;
+using PizzeriaEpiserverSite.Models.Pages;
+using PizzeriaEpiserverSite.Models.ViewModels;
+
+namespace PizzeriaEpiserverSite.Controllers
+{
+    public class BlogCategoryListPageController : PageController<BlogCategoryListPage>
+    {
+        public ActionResult Index(BlogCategoryListPage currentPage, string category)
+        {
+            var model = new BlogCategoryListViewModel(currentPage)
+            {
+                Categories = GetAllCategories(),
+                Pages = FindPagesForCategory(category)
+            };
+
+            return View(model);
+        }
+
+        private List<SitePageData> FindPagesForCategory(string category)
+        {
+
+            return new List<SitePageData>();
+        }
+
+        private List<Category> GetAllCategories()
+        {
+            var categoryRepository = ServiceLocator.Current.GetInstance<CategoryRepository>();
+            var root = categoryRepository.GetRoot();
+            var children = root.Categories;
+            return children.Select(x => x).ToList();
+        }
+    }
+}
---
+++ PizzeriaEpiserverSite/Models/ViewModels/BlogCategoryListViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EPiServer.DataAbstraction;
+using PizzeriaEpiserverSite.Models.Pages;
+
+namespace PizzeriaEpiserverSite.Models.ViewModels
+{
+    public class BlogCategoryListViewModel : IPageViewModel<BlogCategoryListPage>
+    {
+        public List<Category> Categories { get; set; }
+        public List<SitePageData> Pages { get; set; }
+        public BlogCategoryListPage CurrentPage { get; }
+        public BlogCategoryListViewModel(BlogCategoryListPage currentPage)
+        {
+            CurrentPage = currentPage;
+        }
+    }
+}
